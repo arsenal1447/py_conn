9c9e93b9e4f2a82832f97d061c34a316e18eeac8
xinxin <xinxin@rrkd.cn>
2016-08-08 17:12:05 +0800

    add imgs

--- conn.py
+++ conn.py
@@ -9,17 +9,17 @@ try:
     conn.select_db('python')
     cur.execute("create table test(id int,info varchar(20))")
 
-    value=[1,'h1 rollen']
+    value=[1,'h1 kent']
     cur.execute("insert into test values(%s,%s)",value)
 
     values = [] 
     for i in range(20):
-        values.append((i,'hi rollen'+str(i)))
+        values.append((i,'hi kent'+str(i)))
 
 
     cur.executemany('insert into test values(%s,%s)',values)
 
-    cur.execute('update test set info="I am rollen" where id=3')
+    cur.execute('update test set info="I am kent" where id=3')
 
     conn.commit()
 
